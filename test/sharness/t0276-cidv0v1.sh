56a3bafe5e8bb72073e9ba318c5d0e6c3028904f
Kevin Atkinson <k@kevina.org>
2018-07-28 02:48:29 -0400

    Use better names for hash variables.
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- test/sharness/t0276-cidv0v1.sh
+++ test/sharness/t0276-cidv0v1.sh
@@ -20,25 +20,25 @@ test_expect_success "create two small files" '
 '
 
 test_expect_success "add file using CIDv1 but don't pin" '
-  AHASH_CIDv1=$(ipfs add -q --cid-version=1 --raw-leaves=false --pin=false afile)
+  AHASHv1=$(ipfs add -q --cid-version=1 --raw-leaves=false --pin=false afile)
 '
 
 test_expect_success "add file using CIDv0" '
-  AHASH_CIDv0=$(ipfs add -q --cid-version=0 afile)
+  AHASHv0=$(ipfs add -q --cid-version=0 afile)
 '
 
 test_expect_success "check hashes" '
-  test "$(cid-fmt %v-%c $AHASH_CIDv0)" = "cidv0-protobuf" &&
-  test "$(cid-fmt %v-%c $AHASH_CIDv1)" = "cidv1-protobuf" &&
-  test "$(cid-fmt -v 0 %s $AHASH_CIDv1)" = "$AHASH_CIDv0"
+  test "$(cid-fmt %v-%c $AHASHv0)" = "cidv0-protobuf" &&
+  test "$(cid-fmt %v-%c $AHASHv1)" = "cidv1-protobuf" &&
+  test "$(cid-fmt -v 0 %s $AHASHv1)" = "$AHASHv0"
 '
 
 test_expect_success "make sure CIDv1 hash really is in the repo" '
-  ipfs refs local | grep -q $AHASH_CIDv1
+  ipfs refs local | grep -q $AHASHv1
 '
 
 test_expect_success "make sure CIDv0 hash really is in the repo" '
-  ipfs refs local | grep -q $AHASH_CIDv0
+  ipfs refs local | grep -q $AHASHv0
 '
 
 test_expect_success "run gc" '
@@ -46,22 +46,22 @@ test_expect_success "run gc" '
 '
 
 test_expect_success "make sure the CIDv0 hash is in the repo" '
-  ipfs refs local | grep -q $AHASH_CIDv0
+  ipfs refs local | grep -q $AHASHv0
 '
 
 test_expect_success "make sure we can get CIDv0 added file" '
-  ipfs cat $AHASH_CIDv0 > thefile &&
+  ipfs cat $AHASHv0 > thefile &&
   test_cmp afile thefile
 '
 
 test_expect_success "make sure the CIDv1 hash is not in the repo" '
-  ! ipfs refs local | grep -q $AHASH_CIDv1
+  ! ipfs refs local | grep -q $AHASHv1
 '
 
 test_expect_success "clean up" '
-  ipfs pin rm $AHASH_CIDv0 &&
+  ipfs pin rm $AHASHv0 &&
   ipfs repo gc &&
-  ! ipfs refs local | grep -q $AHASH_CIDv0
+  ! ipfs refs local | grep -q $AHASHv0
 '
 
 #
@@ -73,7 +73,7 @@ test_expect_success "add file using CIDv1 but don't pin" '
 '
 
 test_expect_success "check that we can access the file when converted to CIDv0" '
-  ipfs cat $AHASH_CIDv0 > thefile &&
+  ipfs cat $AHASHv0 > thefile &&
   test_cmp afile thefile
 '
 
@@ -86,7 +86,7 @@ test_expect_success "add file using CIDv0 but don't pin" '
 '
 
 test_expect_success "check that we can access the file when converted to CIDv1" '
-  ipfs cat $AHASH_CIDv1 > thefile &&
+  ipfs cat $AHASHv1 > thefile &&
   test_cmp afile thefile
 '
 
@@ -108,17 +108,17 @@ test_expect_success "add afile using CIDv0 to node 0" '
 '
 
 test_expect_failure "get afile using CIDv1 via node 1" '
-  iptb run 1 ipfs --timeout=2s cat $AHASH_CIDv1 > thefile &&
+  iptb run 1 ipfs --timeout=2s cat $AHASHv1 > thefile &&
   test_cmp afile thefile
 '
 
 test_expect_success "add bfile using CIDv1 to node 0" '
-  BHASH_CIDv1=$(iptb run 0 ipfs add -q --cid-version=1 --raw-leaves=false bfile)
+  BHASHv1=$(iptb run 0 ipfs add -q --cid-version=1 --raw-leaves=false bfile)
 '
 
 test_expect_failure "get bfile using CIDv0 via node 1" '
-  BHASH_CIDv0=$(cid-fmt -v 0 %s $BHASH_CIDv1)
-  iptb run 1 ipfs --timeout=2s cat $BHASH_CIDv0 > thefile &&
+  BHASHv0=$(cid-fmt -v 0 %s $BHASHv1)
+  iptb run 1 ipfs --timeout=2s cat $BHASHv0 > thefile &&
   test_cmp bfile thefile
 '
 
