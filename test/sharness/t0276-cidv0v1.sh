90ffef85de3b514b2057b239614efbdee6141962
Kevin Atkinson <k@kevina.org>
2018-07-28 02:27:55 -0400

    Use 'cid-fmt' utility to avoid hardcoding hashes.
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- test/sharness/t0276-cidv0v1.sh
+++ test/sharness/t0276-cidv0v1.sh
@@ -14,33 +14,31 @@ test_init_ipfs
 #
 #
 
-test_expect_success "create a small file" '
+test_expect_success "create two small files" '
   random 1000 7 > afile
+  random 1000 9 > bfile
 '
 
 test_expect_success "add file using CIDv1 but don't pin" '
-  HASH1=$(ipfs add -q --cid-version=1 --raw-leaves=false --pin=false afile)
+  AHASH_CIDv1=$(ipfs add -q --cid-version=1 --raw-leaves=false --pin=false afile)
 '
 
 test_expect_success "add file using CIDv0" '
-  HASH0=$(ipfs add -q --cid-version=0 afile)
+  AHASH_CIDv0=$(ipfs add -q --cid-version=0 afile)
 '
 
-## note the only difference between these two hashes are the cid
-## version the multihash must be the same and they must both be using
-## a protobuf
 test_expect_success "check hashes" '
-  echo $HASH0 $HASH1 &&
-  test "$HASH0" = QmUUmUz49Yj3XsPz397V1cFh4wXW7PhYAr8F1YMgw3Qrpb &&
-  test "$HASH1" = zdj7Wba1rR812CeQahumt56sz6Fh9iwpw7g3AXmgLkMkUykzX
+  test "$(cid-fmt %v-%c $AHASH_CIDv0)" = "cidv0-protobuf" &&
+  test "$(cid-fmt %v-%c $AHASH_CIDv1)" = "cidv1-protobuf" &&
+  test "$(cid-fmt -v 0 %s $AHASH_CIDv1)" = "$AHASH_CIDv0"
 '
 
 test_expect_success "make sure CIDv1 hash really is in the repo" '
-  ipfs refs local | grep -q $HASH1
+  ipfs refs local | grep -q $AHASH_CIDv1
 '
 
 test_expect_success "make sure CIDv0 hash really is in the repo" '
-  ipfs refs local | grep -q $HASH0
+  ipfs refs local | grep -q $AHASH_CIDv0
 '
 
 test_expect_success "run gc" '
@@ -48,22 +46,22 @@ test_expect_success "run gc" '
 '
 
 test_expect_success "make sure the CIDv0 hash is in the repo" '
-  ipfs refs local | grep -q $HASH0
+  ipfs refs local | grep -q $AHASH_CIDv0
 '
 
 test_expect_success "make sure we can get CIDv0 added file" '
-  ipfs cat $HASH0 > thefile &&
+  ipfs cat $AHASH_CIDv0 > thefile &&
   test_cmp afile thefile
 '
 
 test_expect_success "make sure the CIDv1 hash is not in the repo" '
-  ! ipfs refs local | grep -q $HASH1
+  ! ipfs refs local | grep -q $AHASH_CIDv1
 '
 
 test_expect_success "clean up" '
-  ipfs pin rm $HASH0 &&
+  ipfs pin rm $AHASH_CIDv0 &&
   ipfs repo gc &&
-  ! ipfs refs local | grep -q $HASH0
+  ! ipfs refs local | grep -q $AHASH_CIDv0
 '
 
 #
@@ -75,7 +73,7 @@ test_expect_success "add file using CIDv1 but don't pin" '
 '
 
 test_expect_success "check that we can access the file when converted to CIDv0" '
-  ipfs cat $HASH0 > thefile &&
+  ipfs cat $AHASH_CIDv0 > thefile &&
   test_cmp afile thefile
 '
 
@@ -88,18 +86,13 @@ test_expect_success "add file using CIDv0 but don't pin" '
 '
 
 test_expect_success "check that we can access the file when converted to CIDv1" '
-  ipfs cat $HASH1 > thefile &&
+  ipfs cat $AHASH_CIDv1 > thefile &&
   test_cmp afile thefile
 '
 
 #
 #
 #
-
-test_expect_success "create another small file" '
-  random 1000 9 > bfile
-'
-
 
 test_expect_success "set up iptb testbed" '
   iptb init -n 2 -p 0 -f --bootstrap=none
@@ -115,21 +108,17 @@ test_expect_success "add afile using CIDv0 to node 0" '
 '
 
 test_expect_failure "get afile using CIDv1 via node 1" '
-  iptb run 1 ipfs --timeout=2s cat $HASH1 > thefile &&
+  iptb run 1 ipfs --timeout=2s cat $AHASH_CIDv1 > thefile &&
   test_cmp afile thefile
 '
 
 test_expect_success "add bfile using CIDv1 to node 0" '
-  HASHb1=$(iptb run 0 ipfs add -q --cid-version=1 --raw-leaves=false bfile)
-'
-
-test_expect_success "check hash" '
-  test "$HASHb1" = "zdj7WZvSpXAHwwdAJK3dSqUpeBtz3JHWtWpakfts5Uygq59p2"
-  HASHb0=QmSqCT66SUU2Hb17thsrxGMLMq6qoM6giSGNCH6JsPWFe6
+  BHASH_CIDv1=$(iptb run 0 ipfs add -q --cid-version=1 --raw-leaves=false bfile)
 '
 
 test_expect_failure "get bfile using CIDv0 via node 1" '
-  iptb run 1 ipfs --timeout=2s cat $HASHb0 > thefile &&
+  BHASH_CIDv0=$(cid-fmt -v 0 %s $BHASH_CIDv1)
+  iptb run 1 ipfs --timeout=2s cat $BHASH_CIDv0 > thefile &&
   test_cmp bfile thefile
 '
 
