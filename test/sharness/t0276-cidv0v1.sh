3547916b7e4ed4256a81cbea9e7d1cedea9922ec
Kevin Atkinson <k@kevina.org>
2018-07-28 00:28:58 -0400

    Add (failing) tests for bitswap.
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- test/sharness/t0276-cidv0v1.sh
+++ test/sharness/t0276-cidv0v1.sh
@@ -92,4 +92,49 @@ test_expect_success "check that we can access the file when converted to CIDv1" 
   test_cmp afile thefile
 '
 
+#
+#
+#
+
+test_expect_success "create another small file" '
+  random 1000 9 > bfile
+'
+
+
+test_expect_success "set up iptb testbed" '
+  iptb init -n 2 -p 0 -f --bootstrap=none
+'
+
+test_expect_success "start nodes" '
+  iptb start &&
+  iptb connect 0 1
+'
+
+test_expect_success "add afile using CIDv0 to node 0" '
+  iptb run 0 ipfs add -q --cid-version=0 afile
+'
+
+test_expect_failure "get afile using CIDv1 via node 1" '
+  iptb run 1 ipfs --timeout=2s cat $HASH1 > thefile &&
+  test_cmp afile thefile
+'
+
+test_expect_success "add bfile using CIDv1 to node 0" '
+  HASHb1=$(iptb run 0 ipfs add -q --cid-version=1 --raw-leaves=false bfile)
+'
+
+test_expect_success "check hash" '
+  test "$HASHb1" = "zdj7WZvSpXAHwwdAJK3dSqUpeBtz3JHWtWpakfts5Uygq59p2"
+  HASHb0=QmSqCT66SUU2Hb17thsrxGMLMq6qoM6giSGNCH6JsPWFe6
+'
+
+test_expect_failure "get bfile using CIDv0 via node 1" '
+  iptb run 1 ipfs --timeout=2s cat $HASHb0 > thefile &&
+  test_cmp bfile thefile
+'
+
+test_expect_success "stop testbed" '
+  iptb stop
+'
+
 test_done
